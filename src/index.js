ea65f8a46081b6e2a545f36ac309e2f0f0bb8470
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-03-04 09:24:20 -0300

    first steps in react

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { store } from '@redux-toolkit/store'; // almacen de datos del estado glo
 import App from '@root/App';
 import '@root/index.scss';
 
+// archivo de entrada a la app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
